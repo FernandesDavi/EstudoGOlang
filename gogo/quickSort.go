c84cb716aad6e9cb06f3ef1892a15e220d33aa8e
Davi Fernandes <t_davif1@verifone.com>
2017-07-31 15:27:20 -0300

    order

--- gogo/quickSort.go
+++ gogo/quickSort.go
@@ -4,19 +4,7 @@ import (
 	"os"
 	"strconv"
 )
-func quicksort(numeros []int) []int{
-	if len(numeros) <= 1{
-		return numeros
-	}
-	//criar uma copia do vertor original
-	n := make([]int, len(numeros))
-	copy(n, numeros)
 
-	//Criar o pivo da lista
-	indPivo := len(n) / 2
-	pivo := n[indPivo]
-
-}
 func main(){
 	entrada := os.Args[1:]
 	numeros := make([]int, len(entrada))
@@ -30,4 +18,33 @@ func main(){
 		numeros[i] = numero
 	}
 	fmt.Println(quicksort(numeros))
+}
+
+func quicksort(numeros []int) []int{
+	if len(numeros) <= 1{
+		return numeros
+	}
+	//criar uma copia do vertor original
+	n := make([]int, len(numeros))
+	copy(n, numeros)
+
+	//Criar o pivo da lista
+	indPivo := len(n) / 2
+	pivo := n[indPivo]
+	n = append(n[:indPivo], n[indPivo+1:]...)
+	menores, maiores := particionar(n, pivo)
+	return append(append(quicksort(menores), pivo), quicksort(maiores)...)
+	
+}
+
+func particionar(numeros []int, pivo int) (menores []int, maiores []int) {
+
+	for _, n := range numeros{
+		if n <= pivo{
+			menores = append(menores, n)
+		}else{
+			maiores = append(maiores, n)
+		}
+	}
+	return menores, maiores
 }
